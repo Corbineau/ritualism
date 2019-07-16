ed11c7aaa4706d021d38b7ae08303bfd13a46b7b
corbineau <lochinvar@gmail.com>
2019-07-16 14:55:14 -0400

    new dependencies

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use("./routes/apiRoutes");
+app.use("./routes/htmlRoutes");
 
 // =============================================================================
 // LISTENER
